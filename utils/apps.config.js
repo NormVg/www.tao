45992b3f7486af5d0bcb5b44dc8a2ca7e6af14cb
Vishnu Gupta <thenormvg@gmail.com>
2025-07-06 13:10:22 +0530

    fix something

--- utils/apps.config.js
+++ utils/apps.config.js
@@ -6,13 +6,6 @@ const appDataConfig = [
     link:"https://cook.taohq.org",
     sub:"Cook is a powerful, extensible CLI tool designed to streamline and  automate everyday development tasks. Whether you're setting up a new  project, managing boilerplate code, running repetitive commands, or  tunneling a local server — Cook brings it all together in one elegant  toolkit. Built for developers who prefer building over boilerplate, it  helps you move faster with less setup and more shipping.",
     thumb:cookThumb
-  },
-  {
-    name:"COOK",
-    link:"https://cook.taohq.org",
-    sub:"Cook is a powerful, extensible CLI tool designed to streamline and  automate everyday development tasks. Whether you're setting up a new  project, managing boilerplate code, running repetitive commands, or  tunneling a local server — Cook brings it all together in one elegant  toolkit. Built for developers who prefer building over boilerplate, it  helps you move faster with less setup and more shipping.",
-    thumb:cookThumb
-
   }
 ]
 
