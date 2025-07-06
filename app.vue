db5569e1995f35bcb214773ed34784ccc7589c29
Vishnu Gupta <thenormvg@gmail.com>
2025-07-06 13:06:55 +0530

    added the seo

--- app.vue
+++ app.vue
@@ -3,14 +3,19 @@
 </template>
 <script setup>
 useSeoMeta({
-  title: 'TheAlphaOnes Docs',
-  description: 'Explore documentation for all tools by TheAlphaOnes.',
-  ogTitle: 'TheAlphaOnes Docs',
-  ogDescription: 'Guide and Documentation by TheAlphaOnes.',
-  ogImage: '/og.png',
+  title: 'TheAlphaOnes — Building the Future of Software',
+  description: 'A startup-in-the-making, crafting developer tools, AI projects, and bold ideas shaping the next generation of technology.',
+  ogTitle: 'TheAlphaOnes — Bold Tools, AI Projects & More',
+  ogDescription: 'Discover TheAlphaOnes — a startup shaping innovative developer tools, AI experiments, and futuristic software experiences. We also host a small creative community.',
+  ogImage: 'https://www.taohq.org/og.png',
   ogType: 'website',
   twitterCard: 'summary_large_image',
   twitterSite: '@thenormvg',
   themeColor: '#18171C',
+  keywords: 'developer tools, AI projects, software startup, innovative tech, future of software, AI experiments, creative software',
+  author: 'TheAlphaOnes',
+  creator: 'TheAlphaOnes',
+  robots: 'index, follow',
 })
+
 </script>
