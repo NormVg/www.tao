fffddbeff26d554861e8ea575a7fd29f00a0c704
Vishnu_Mac <thenormvg@gmail.com>
2025-10-23 12:13:59 +0530

    added orbit

--- utils/apps.config.js
+++ utils/apps.config.js
@@ -1,13 +1,20 @@
 import cookThumb from "~/assets/cook-thumb.png"
+import orbitThumb from "~/assets/orbit-thumb.png"
 
 const appDataConfig = [
   {
-    name:"COOK",
-    link:"https://cook.taohq.org",
-    sub:"Cook is a powerful, extensible CLI tool designed to streamline and  automate everyday development tasks. Whether you're setting up a new  project, managing boilerplate code, running repetitive commands, or  tunneling a local server — Cook brings it all together in one elegant  toolkit. Built for developers who prefer building over boilerplate, it  helps you move faster with less setup and more shipping.",
-    thumb:cookThumb
+    name: "COOK",
+    link: "https://cook.taohq.org",
+    sub: "Cook is a powerful, extensible CLI tool designed to streamline and  automate everyday development tasks. Whether you're setting up a new  project, managing boilerplate code, running repetitive commands, or  tunneling a local server — Cook brings it all together in one elegant  toolkit. Built for developers who prefer building over boilerplate, it  helps you move faster with less setup and more shipping.",
+    thumb: cookThumb
+  },
+  {
+    name: "ORBIT.MUJ",
+    link: "https://orbit.taohq.org",
+    sub: "Orbit.MUJ is a private community for the 2025 batch at Manipal University Jaipur - a space for builders, founders, and creators who act now. It’s invite only, focused on collaboration, skill, and clarity of purpose. No titles, no hype just momentum.",
+    thumb: orbitThumb
   }
 ]
 
 
-export {appDataConfig}
+export { appDataConfig }
