a517b3e0e80fd0c28a4ac54b4447411787e281db
Vishnu_Mac <thenormvg@gmail.com>
2025-12-17 23:52:24 +0530

    added images

--- utils/team.config.js
+++ utils/team.config.js
@@ -4,17 +4,38 @@ const foundingMembers = [
     name: 'Vishnu',
     role: 'Founder & CEO',
     website: 'https://vishnu.taohq.org',
-    image: '/team/vishnu.jpg', // Add your image to /public/team/
-    bio: 'Building the future of developer tools'
+    image: '/team/vishnu.png',
   },
   {
-    name: 'Vishnu',
-    role: 'Founder & CEO',
+    name: 'Akshat',
+    role: 'Co-Founder & COO',
     website: 'https://vishnu.taohq.org',
-    image: '/team/vishnu.jpg', // Add your image to /public/team/
-    bio: 'Building the future of developer tools'
+    image: '/team/akshat.jpeg',
   },
-
-]
+  {
+    name: 'Saurav',
+    role: 'Co-Founder & CTO',
+    website: 'https://vishnu.taohq.org',
+    image: '/team/saurav.jpeg',
+  },
+  {
+    name: 'Parv',
+    role: 'CFO & Marketing Lead',
+    website: 'https://www.linkedin.com/in/parv-srivastava-a29435259/',
+    image: '/team/parv.png',
+  },
+  {
+    name: 'Sibam',
+    role: 'Full Stack Engineer',
+    website: 'https://www.linkedin.com/in/sibam-paul-b717b4281/',
+    image: '/team/sibam.jpeg',
+  },
+  {
+    name: 'Shubham',
+    role: 'Frontend Engineer',
+    website: 'https://www.linkedin.com/in/shubham-bansal404/',
+    image: '/team/bansal.jpeg',
+  },
+];
 
 export { foundingMembers }
